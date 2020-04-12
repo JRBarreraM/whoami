0ebc297aa5d57f37c527519c711e0fe57558a5e4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-12 15:16:28 +0000

    Deploy JRBarreraM/whoami to github.com/JRBarreraM/whoami.git:gh-pages

---
+++ precache-manifest.84ea450eba3fa7e960e8dd99adf828f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27b0b0f4b99ca576f877bb39f02d0d3",
+    "url": "/whoami/index.html"
+  },
+  {
+    "revision": "07cb2af11be810473b97",
+    "url": "/whoami/static/css/main.7790c032.chunk.css"
+  },
+  {
+    "revision": "5afd1dec34dbf091501b",
+    "url": "/whoami/static/js/2.4571a8ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whoami/static/js/2.4571a8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cb2af11be810473b97",
+    "url": "/whoami/static/js/main.5db449ac.chunk.js"
+  },
+  {
+    "revision": "81b06ada0f1d75a761bf",
+    "url": "/whoami/static/js/runtime-main.ebe46dfb.js"
+  }
+]);
